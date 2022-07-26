2311d0d45ab503da757655693b8506aa0a530952
Taranpreet Singh <taranpreetsingh902@gmail.com>
2022-07-26 17:49:01 +0530

    Done

--- frontend/main.js
+++ frontend/main.js
@@ -1,20 +1,3 @@
-let loginBtn = document.getElementById("login-btn");
-let logoutBtn = document.getElementById("logout-btn");
-
-let token = localStorage.getItem("token");
-
-if (token) {
-  loginBtn.remove();
-} else {
-  logoutBtn.remove();
-}
-
-logoutBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  localStorage.removeItem("token");
-  window.location = "file:///C:/Users/Dennis%20Ivy/Desktop/frontend/login.html";
-});
-
 let projectsUrl = "http://127.0.0.1:8000/api/projects/";
 
 let getProjects = () => {
